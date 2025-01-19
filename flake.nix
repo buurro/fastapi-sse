30781a9dcc9df9e075930d026f6196cdb2945076
Marco Burro <marcoburro98@gmail.com>
2025-01-19 12:27:32 +0100

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
@@ -24,14 +25,16 @@
   outputs =
     { self
     , nixpkgs
+    , flake-utils
     , uv2nix
     , pyproject-nix
     , pyproject-build-systems
     , ...
-    }:
+    }: flake-utils.lib.eachDefaultSystem (system:
     let
       inherit (nixpkgs) lib;
-      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+
+      pkgs = nixpkgs.legacyPackages.${system};
 
       workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
 
@@ -39,48 +42,37 @@
         sourcePreference = "wheel";
       };
 
-      # Python sets grouped per system
-      pythonSets = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
+      baseSet = pkgs.callPackage pyproject-nix.build.packages {
+        python = pkgs.python313;
+      };
 
-          # Base Python package set from pyproject.nix
-          baseSet = pkgs.callPackage pyproject-nix.build.packages {
-            python = pkgs.python313;
-          };
-        in
-        baseSet.overrideScope (
-          lib.composeManyExtensions [
-            pyproject-build-systems.overlays.default
-            overlay
-          ]
-        )
+      pythonSet = baseSet.overrideScope (
+        lib.composeManyExtensions [
+          pyproject-build-systems.overlays.default
+          overlay
+        ]
       );
+
+      venv = pythonSet.mkVirtualEnv "fastapi-sse-env" workspace.deps.default;
     in
     {
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          pythonSet = pythonSets.${system};
-          venv = pythonSet.mkVirtualEnv "fastapi-sse-env" workspace.deps.default;
-        in
-        lib.optionalAttrs pkgs.stdenv.isLinux {
-          containerImage = pkgs.dockerTools.streamLayeredImage {
-            name = "ghcr.io/buurro/fastapi-sse";
-            tag = "latest";
-            contents = [
-              venv
-              pkgs.busybox
-            ];
-            config = {
-              Cmd = [ "uvicorn" "fastapi_sse.main:app" "--host=0.0.0.0" ];
-              ExposedPorts = {
-                "8000/tcp" = { };
-              };
+      packages = {
+        default = venv;
+      } // lib.optionalAttrs pkgs.stdenv.isLinux {
+        containerImage = pkgs.dockerTools.streamLayeredImage {
+          name = "ghcr.io/buurro/fastapi-sse";
+          tag = "latest";
+          contents = [
+            venv
+            pkgs.busybox
+          ];
+          config = {
+            Cmd = [ "uvicorn" "fastapi_sse.main:app" "--host=0.0.0.0" ];
+            ExposedPorts = {
+              "8000/tcp" = { };
             };
           };
-        }
-      );
-    };
+        };
+      };
+    });
 }
